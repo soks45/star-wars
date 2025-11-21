0f52c630c8e83ec5f869f3f1f7b5987b391886a0
Aleksey Batuev <batuev.dev@gmail.com>
2025-11-21 17:29:33 +0500

    chore: added taiga

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
+
+import { provideEventPlugins } from '@taiga-ui/event-plugins';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+    providers: [
+        provideAnimations(), // tui import
+        provideBrowserGlobalErrorListeners(),
+        provideRouter(routes),
+        provideEventPlugins(),
+    ],
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { TuiRoot } from '@taiga-ui/core';
+
 @Component({
     selector: 'sw-root',
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, TuiRoot],
     templateUrl: './app.html',
     styleUrl: './app.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
